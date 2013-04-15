7d635c503c02c60669b9c9aaf63776bd45577b17
Hoang The Hung <hth321@gmail.com>
2013-04-15 16:26:52 +0700

    edit user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130415044519) do
+ActiveRecord::Schema.define(:version => 20130415090825) do
 
   create_table "branch", :force => true do |t|
     t.string  "name",                                                :null => false
@@ -514,6 +514,7 @@ ActiveRecord::Schema.define(:version => 20130415044519) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "avatar"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
